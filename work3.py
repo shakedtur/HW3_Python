e696519b5194869a9035cd84164d6eedae40a1a1
shakedtur <59848826+shakedtur@users.noreply.github.com>
2020-12-09 15:56:29 +0200

    include partB ex1

--- work3.py
+++ work3.py
@@ -36,18 +36,22 @@ def diagonal(r):
     return sqrt(length(r)**2+width(r)**2)
 
 def print_rectangle(r):
+    """printing the rectangle as point=(x,y) ; size= lenXwid"""
     return 'Rectangle: point=({0},{1}); size= {2}X{3}'.format(x(r),y(r),length(r),width(r))
 
 def center(r):
+    """calc the center point of the rectangle"""
     return (x(r)+length(r)/2,(y(r)+width(r)+y(r))/2)
 
 def distance(r1,r2):
+    """calculate the distance between 2 center of 2 retangles"""
     from math import sqrt
     subX=(center(r1)[0]-center(r2)[0])**2
     subY=(center(r1)[1]-center(r2)[1])**2
     return sqrt(subX+subY)
 
 def move(r,deltaX,deltaY):
+    """create a new rectangle and move it according deltaX ,deltaY"""
     return make_rectangle(x(r)+deltaX,y(r)+deltaY,length(r),width(r))
 
 def resize(r,factor):
@@ -69,7 +73,9 @@ print(print_rectangle(resize(r1,0.5)))
 
 #part B
 
+#ex1
 def make_pair(x,y):
+    """func to crate a pair """
     def dispatch1(i):
         if i==0:
             return p[0]
@@ -84,33 +90,46 @@ def get_item_pair(p,i):
 
 from functools import reduce
 
-def add(a,b):
-    return a+b
+# def add(a,b):
+#     """func sum of 2 values"""
+#     return a+b
 
-def avgrage1(list_of_values):
+def average1(list_of_values):
+    """calculate average of list"""
     return sum(list_of_values)/len(list_of_values)
 
 def merage(one,two):
-    return (one,two)
-#need fix
+    """return to valus as a sequence"""
+    return one,two
+
 def avg_grades(sequence1):
-    avg=tuple(map(avgrage1, map(secend_value_In_pair, sequence1)))
-    seq2=tuple(map(first_value_In_pair,courses))
-    from functools import reduce
-    return reduce(merage,avg)
+    """function to calculate average grades by courses name and merage them"""
+    avg=(map(average1, map(secend_value_In_pair, sequence1)))#split the courses grades to other list and calc the averge
+    seq2=(map(first_value_In_pair,courses))#split the courses names
+    return map(merage,seq2,avg)#merage the match data to pair
+
 
 def first_value_In_pair(pairs):
     return pairs[0]
+
 def secend_value_In_pair(pairs):
     return pairs[1]
 
-reshima=[10,5,3]
-print(avgrage1(reshima))
-
-
 courses = (('a', [81, 78, 57])), ('b', [95, 98]), ('c', [75, 45]), ('d', [58])
+factors= (('c',15),('a',20))
+
 
 print(tuple(map(first_value_In_pair,courses)))
-print(avg_grades(courses))
-print(map(avgrage1,courses))
+print("avg",tuple(avg_grades(courses)))
+print(map(average1, courses))
+
+
+#ex2
+
+def add_factors(seq1,seq2):
+    pass
+
+
+
+
 

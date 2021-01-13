5acc240acf2823f12829fd826d71ca8ac4d36c6f
shakedtur <59848826+shakedtur@users.noreply.github.com>
2021-01-13 16:38:49 +0200

    ex 4+5

--- ass4.py
+++ ass4.py
@@ -1,68 +1,10 @@
-from fractions import gcd
-from math import atan2, sin, cos,pi
+### Q2 - Q6 ###: see solutions of past exams
 
-####################
-# Rational numbers #
-####################
+### Q9 - Q10 ###: see solutions of past exams
 
-def add_rational(x, y):
-    """Add rational numbers x and y."""
-    nx, dx = x.numer, x.denom
-    ny, dy = y.numer, y.denom
-    return Rational(nx * dy + ny * dx, dx * dy)
-
-def mul_rational(x, y):
-    """Multiply rational numbers x and y."""
-    return Rational(x.numer * y.numer, x.denom * y.denom)
-
-
-class Rational(object):
-    """A rational number represented as a numerator and denominator.
-
-    All rational numbers are represented in lowest terms.
-
-    >>> Rational(6, 4)
-    Rational(3, 2)
-    >>> add_rational(Rational(3, 14), Rational(2, 7))
-    Rational(1, 2)
-    >>> mul_rational(Rational(7, 10), Rational(2, 7))
-    Rational(1, 5)
-    """
-
-    def __init__(self, numer, denom):
-        g = gcd(numer, denom)
-        self.numer = numer // g
-        self.denom = denom // g
-
-    def __repr__(self):
-        return 'Rational({0}, {1})'.format(self.numer, self.denom)
-
-    def __str__(self):
-        return '{0}/{1}'.format(self.numer, self.denom)
-
-###################
-# Complex numbers #
-###################
-
-def add_complex(z1, z2):
-    """Return a complex number z1 + z2"""
-    return ComplexRI(z1.real + z2.real, z1.imag + z2.imag)
-
-def mul_complex(z1, z2):
-    """Return a complex number z1 * z2"""
-    return ComplexMA(z1.magnitude * z2.magnitude, z1.angle + z2.angle)
-
-
+### Q7 - Q8 ###:
+from math import atan2, sin, cos, pi,gcd
 class ComplexRI(object):
-    """A rectangular representation of a complex number.
-
-    >>> from math import pi
-    >>> add_complex(ComplexRI(1, 2), ComplexMA(2, pi/2))
-    ComplexRI(1.0000000000000002, 4.0)
-    >>> mul_complex(ComplexRI(0, 1), ComplexRI(0, 1))
-    ComplexMA(1.0, 3.141592653589793)
-    """
-
     def __init__(self, real, imag):
         self.real = real
         self.imag = imag
@@ -77,11 +19,11 @@ class ComplexRI(object):
 
     def __repr__(self):
         return 'ComplexRI({0}, {1})'.format(self.real, self.imag)
-
+##    def __str__(self):
+##        return 'complex number in cartezian representaion: {0} + i*{1}'.format(self.real, self.imag)
+    
 
 class ComplexMA(object):
-    """A polar representation of a complex number."""
-
     def __init__(self, magnitude, angle):
         self.magnitude = magnitude
         self.angle = angle
@@ -97,83 +39,156 @@ class ComplexMA(object):
     def __repr__(self):
         return 'ComplexMA({0}, {1})'.format(self.magnitude, self.angle)
 
+def add_complex(z1, z2):
+    return ComplexRI(z1.real + z2.real, z1.imag + z2.imag)
 
-##############################
-# Tag-based type dispatching #
-##############################
+def mul_complex(z1, z2):
+    return ComplexMA(z1.magnitude * z2.magnitude, z1.angle + z2.angle)
+
+print(add_complex(ComplexRI(1, 2), ComplexMA(2, pi/2)))
+print(mul_complex(ComplexRI(0, 1), ComplexRI(0, 1)))
+##ComplexRI(1.0000000000000002, 4.0)
+##ComplexMA(1.0, 3.141592653589793)
+
+ComplexRI.__add__ = lambda self, other: add_complex(self,other)									
+ComplexMA.__add__ = lambda self, other: add_complex(self,other) 									
+ComplexRI.__mul__ = lambda self, other: mul_complex(self,other) 									
+ComplexMA.__mul__ = lambda self, other: mul_complex(self,other)
+
+print(ComplexRI(1, 2) + ComplexMA(2, 0))
+print(ComplexRI(0, 1) * ComplexRI(0, 1))
+##ComplexRI(3.0, 2.0)
+##ComplexMA(1.0, 3.141592653589793)
+
+
+class Rational(object):
+    def __init__(self, numer, denom):
+        g = gcd(numer, denom)
+        self.numer = numer // g
+        self.denom = denom // g
+
+    def __repr__(self):
+        return 'Rational({0}, {1})'.format(self.numer, self.denom)
+
+def add_rational(x, y):
+    nx, dx = x.numer, x.denom
+    ny, dy = y.numer, y.denom
+    return Rational(nx * dy + ny * dx, dx * dy)
+
+def mul_rational(x, y):
+    return Rational(x.numer * y.numer, x.denom * y.denom)
+
+def iscomplex(z):
+    return type(z) in (ComplexRI, ComplexMA)
+
+def isrational(z):
+    return type(z) == Rational
+
+def add_complex_and_rational(z, r):
+    return ComplexRI(z.real + r.numer/r.denom, z.imag)
 
 def type_tag(x):
-    """Return the tag associated with the type of x."""
     return type_tag.tags[type(x)]
 
-type_tag.tags = {ComplexRI: 'com', ComplexMA: 'com', Rational:  'rat'}
+type_tag.tags = {ComplexRI: 'com', ComplexMA: 'com', Rational: 'rat'}
 
 def add(z1, z2):
-    """Add z1 and z2, which may be complex or rational.
-
-    >>> add(ComplexRI(1.5, 0), Rational(3, 2))
-    ComplexRI(3.0, 0)
-    >>> add(Rational(5, 3), Rational(1, 2))
-    Rational(13, 6)
-    """
     types = (type_tag(z1), type_tag(z2))
     return add.implementations[types](z1, z2)
 
-def add_complex_and_rational(z, r):
-    return ComplexRI(z.real + float(r.numer)/r.denom, z.imag)
-
-add_rational_and_complex = lambda r, z:add_complex_and_rational(z, r)
-
 add.implementations = {}
 add.implementations[('com', 'com')] = add_complex
-add.implementations[('rat', 'rat')] = add_rational
 add.implementations[('com', 'rat')] = add_complex_and_rational
-add.implementations[('rat', 'com')] = add_rational_and_complex
+add.implementations[('rat', 'com')] = lambda x, y: add_complex_and_rational(y, x)
+add.implementations[('rat', 'rat')] = add_rational
 
-############
-# Coercion #
-############
+print(add(ComplexRI(1.5, 0), Rational(3, 2)))
+print(add(Rational(5, 3), Rational(1, 2)))
+##ComplexRI(3.0, 0)
+##Rational(13, 6)
+
+def apply(operator_name, x, y):
+    tags = (type_tag(x), type_tag(y))
+    key = (operator_name, tags)
+    return apply.implementations[key](x, y)
+
+def mul_complex_and_rational(z, r):
+    return ComplexMA(z.magnitude * r.numer / r.denom, z.angle)
+
+mul_rational_and_complex = lambda r, z: mul_complex_and_rational(z, r)
+apply.implementations = {('mul', ('com', 'com')): mul_complex,
+			('mul', ('com', 'rat')): mul_complex_and_rational,
+			('mul', ('rat', 'com')): mul_rational_and_complex,
+			('mul', ('rat', 'rat')): mul_rational}
+adders = add.implementations.items()
+apply.implementations.update({('add', tags):fn for (tags, fn) in adders})
+
+print(apply('add', ComplexRI(1.5, 0), Rational(3, 2)))
+##ComplexRI(3.0, 0)
+print(apply('mul', Rational(1, 2), ComplexMA(10, 1)))
+##ComplexMA(5.0, 1)
+
+class Exponential():
+    def __init__(self, base, power):
+        self.base = base
+        self.power = power
+
+    def __repr__(self):
+        return 'Exponential({0},{1})'.format(self.base, self.power)
+
+type_tag.tags[Exponential] = 'exp'
+
+def mul_exponential_and_complex(e, c):
+    return ComplexMA(e.base*pow(10,e.power)*c.magnitude, c.angle)
+
+apply.implementations[('mul',('exp','com'))] = mul_exponential_and_complex
+apply.implementations[('mul',('com','exp'))] = lambda x,y: mul_exponential_and_complex(y,x)
+
+print(apply('mul', Exponential(2,-4), ComplexMA(10,1)))
+##ComplexMA(0.002, 1)
+print(apply('mul', Exponential(2,-4), ComplexRI(3,4)))
+##ComplexMA(0.001, 0.9272952180016122)
 
 def rational_to_complex(x):
     return ComplexRI(x.numer/x.denom, 0)
+def exponential_to_complex(x):
+    return ComplexRI(x.base*pow(10, x.power), 0)
 
 coercions = {('rat', 'com'): rational_to_complex}
+coercions[('exp','com')] = exponential_to_complex
 
 def coerce_apply(operator_name, x, y):
-    """Apply an operation ('add' or 'mul') to x and y.
-
-    >>> coerce_apply('add', ComplexRI(1.5, 0), Rational(3, 2))
-    ComplexRI(3.0, 0)
-    >>> coerce_apply('mul', Rational(1, 2), ComplexMA(10, 1))
-    ComplexMA(5.0, 1.0)
-    """
     tx, ty = type_tag(x), type_tag(y)
-    if tx != ty:
-        if (tx, ty) in coercions:
-            tx, x = ty, coercions[(tx, ty)](x)
-        elif (ty, tx) in coercions:
-            ty, y = tx, coercions[(ty, tx)](y)
+    if tx != 'com':
+        if (tx, 'com') in coercions:
+            tx, x = 'com', coercions[(tx, 'com')](x)
         else:
-            return 'No coercion possible.'
-    assert tx == ty
+            return 'No coercion possible!'
+    if ty != 'com':
+        if (ty, 'com') in coercions:
+            ty, y = 'com', coercions[(ty, 'com')](y)
+        else:
+            return 'No coercion possible!'
     key = (operator_name, tx)
     return coerce_apply.implementations[key](x, y)
 
-coerce_apply.implementations = {('mul', 'com'): mul_complex,
-                                ('mul', 'rat'): mul_rational,
-                                ('add', 'com'): add_complex,
-                                ('add', 'rat'): add_rational}
+coerce_apply.implementations = {('mul', 'com'):mul_complex,
+				('add', 'com'): add_complex}
+
+print(coerce_apply('add', ComplexRI(1.5, 0), Rational(3, 2)))
+##ComplexRI(3.0, 0)
+print(coerce_apply('mul', Rational(1, 2), ComplexMA(10, 1)))
+##ComplexMA(5.0, 1.0)
+print(coerce_apply('mul', Exponential(2,-4), Rational(1,3)))
+##ComplexMA(6.666666666666667e-05, 0.0)
+print(coerce_apply('mul', Exponential(2,-4), Exponential(3,-5)))
+##ComplexMA(6.000000000000001e-09, 0.0)
 
 
-def driver():
-    #1
-    print (Rational(6, 4))#Rational(3, 2)
-    print (add_rational(Rational(3, 14), Rational(2, 7)))#Rational(1, 2)
-    print (mul_rational(Rational(7, 10), Rational(2, 7)))#  print Rational(1, 5)
-    #2
-    from math import pi
-    print (add_complex(ComplexRI(1, 2), ComplexMA(2, pi/2)))#    ComplexRI(1.0000000000000002, 4.0)
-    print (mul_complex(ComplexRI(0, 1), ComplexRI(0, 1))) #    ComplexMA(1.0, 3.141592653589793)
 
-driver()
+
+
+
+
+
 
